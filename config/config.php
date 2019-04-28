c5a1fa7bcf8993d6c2c3c9d0ee1e5619adc157c6
Your Name <rashidcollins16@gmail.com>
2019-04-28 14:08:23 +0300

    first commit

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect('localhost', 'root', '', 'juma');
+$conn = mysqli_connect('remotemysql.com', 'vpiw5Uhp63', 'W4zwS6og5E', 'vpiw5Uhp63');
 
 if (!$conn) {
   echo "<script>alert('there is an error in with database');</script>";
